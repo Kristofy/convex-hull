9addf4839da876ee40002d7c5bc088da9560134a
Kristof <kristofosztopani@gmail.com>
2021-02-20 16:29:59 +0100

    Updated to a more efficient algorithm, based on polar sort

--- main.js
+++ main.js
@@ -1,92 +1,110 @@
 const padding = 50;
-const percent = 0.4;
 const num = 500;
 
 let points = [];
-let leftMost;
 let hull = [];
-let current;
+let index;
 
-function setup(){
-    let C = createCanvas(windowWidth,windowHeight);
-    // let C = createCanvas(600, 600);
-    let nth = 0;
-    C.parent("parrent");
-
-    // noise
-    
-    for(let i = padding; i < width-padding; i++){
-        for(let j = padding; j < height-padding; j++){
-            let p = noise(i/100,j/100);
-            // console.log(p);
-            if(p<percent){
-                if(nth++%80==0){
-                    points.push(createVector(i,j))
-                }
-            }
-        }
-    }
-   
-	
-    // full random
-/*
-    hull = new Array(num);
-    for(let i = 0; i < num; i++)
-        points.push(createVector(random()*(width-2*padding)+padding, random()*(height-2*padding)+padding));
-*/
-    points.sort((p1, p2) => p1.x-p2.x );
-    leftMost = points.shift();
-    hull.push(leftMost);
-    frameRate(4);
+function turn(c, a, b) {
+  s = (a.x - c.x) * (b.y - c.y) - (a.y - c.y) * (b.x - c.x);
+  return (s > 0) - (s < 0);
 }
 
-function draw(){
-    background(51);
-    
-    current = 0;
-    for(let i = 1; i < points.length; i++){
-        if(vectorMinus(points[current], hull[hull.length-1]).cross(vectorMinus(points[i],hull[hull.length-1])).z > 0){
-            current = i;
-        }
+function setup() {
+  let C = createCanvas(windowWidth, windowHeight);
+
+  C.parent("parrent");
+
+  for (let i = 0; i < num; i++)
+    points.push(
+      createVector(
+        random() * (width - 2 * padding) + padding,
+        random() * (height - 2 * padding) + padding
+      )
+    );
+
+  let bottomLeft = points.shift();
+  points.forEach((x, i) => {
+    if (x.x < bottomLeft.x || (x.x == bottomLeft.x && x.y < bottomLeft.y)) {
+      x = [bottomLeft, (bottomLeft = x)][0];
     }
+  });
 
-    let next = points.splice(current, 1)[0];
+  points.sort((p1, p2) => {
+    const t = turn(bottomLeft, p1, p2);
+    return t > 0 || (t == 0 && (p1.x < p2.x || (p1.x == p2.x && p1.y > p2.y)))
+      ? -1
+      : 1;
+  });
 
-    if(vectorMinus(leftMost, hull[hull.length-1]).cross(vectorMinus(next,hull[hull.length-1])).z<0){
-        noLoop();
-    }else{
-        hull.push(next);
-    }
-
-
-    
-    strokeWeight(1);
-    fill(255,0,0,100);
-    beginShape()
-    hull.forEach(p=>{
-        vertex(p.x, p.y);
-        point(p.x, p.y);
-    });
-    endShape(CLOSE);
-    
-    strokeWeight(5);
-    fill(0,0,255);
-    hull.forEach(p=>{
-        point(p.x, p.y);
-    });
-
-    strokeWeight(6);
-    stroke(0, 255, 0);
-    point(leftMost.x, leftMost.y);
-   
-    strokeWeight(4);
-    stroke(255);
-    points.forEach(p => {
-        point(p.x, p.y);
-    });
-
+  hull.push(points[points.length - 1]);
+  hull.push(bottomLeft);
+  index = 0;
+  //  frameRate(60);
 }
 
-function vectorMinus(v1,v2){
-    return v1.copy().add(p5.Vector.mult(v2.copy(),-1));
+function draw() {
+  background(51, 20);
+
+  strokeWeight(1);
+  noFill();
+  stroke(255, 10);
+  beginShape();
+  for (v of hull) {
+    vertex(v.x, height - v.y);
+  }
+
+  for (let i = index; i < num - 1; i++)
+    vertex(points[i].x, height - points[i].y);
+
+  endShape(CLOSE);
+
+  noStroke();
+  fill(0, 255, 0);
+
+  for (let i = index; i < num - 1; i++)
+    ellipse(points[i].x, height - points[i].y, 1, 1);
+
+  strokeWeight(2);
+  noFill();
+  stroke(190, 0, 190);
+  beginShape();
+  for (v of hull) {
+    vertex(v.x, height - v.y);
+  }
+  endShape(CLOSE);
+
+  noStroke();
+  fill(190, 100, 255);
+  for (v of hull) {
+    ellipse(v.x, height - v.y, 2, 2);
+  }
+
+  if (points.length == index) {
+    return;
+  }
+
+  strokeWeight(2);
+
+  if (hull.length >= 2 && turn(hull[hull.length - 2], hull[hull.length - 1], points[index]) < 0) {
+    stroke(255, 0, 0);
+    line(
+      hull[hull.length - 1].x,
+      height - hull[hull.length - 1].y,
+      points[index].x,
+      height - points[index].y
+    );
+    hull.pop();
+    return;
+  }
+
+  stroke(0, 0, 255);
+  line(
+    hull[hull.length - 1].x,
+    height - hull[hull.length - 1].y,
+    points[index].x,
+    height - points[index].y
+  );
+  hull.push(points[index]);
+  index++;
 }
